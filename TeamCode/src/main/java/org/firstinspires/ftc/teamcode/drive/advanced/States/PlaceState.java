2508c649e40ce68d19296f6fed4145890655bfdb
Kroissant <72971349+kroisssant@users.noreply.github.com>
2023-09-27 20:28:58 +0300

    masd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/States/PlaceState.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.drive.advanced.States;
+
+import org.firstinspires.ftc.teamcode.Libs.STATE;
+
+public class PlaceState implements STATE {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/States/Trajectorys.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/States/Trajectorys.java
@@ -9,8 +9,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class Trajectorys {
     SampleMecanumDrive drive;
 
-    Trajectory toAlliceYellow;
-    Trajectory toBackDrop1;
+    Trajectory toAlliceYellow, toBackDrop1, toStack1, toStack2, toStack3;
 
     Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
 
@@ -26,5 +25,20 @@ public class Trajectorys {
         toBackDrop1 = drive.trajectoryBuilder(toAlliceYellow.end())
                 .lineToLinearHeading(new Pose2d(0,0, 80))
                 .build();
+
+        // other 2 for the other 2 posible posotion
+
+        // Stack Trajectoys
+         toStack1 =  drive.trajectoryBuilder(toBackDrop1.end())
+                 .lineToLinearHeading(new Pose2d(0, 0, 70))
+                 .build();
+         toStack2 = drive.trajectoryBuilder(toStack1.end())
+                 .lineToLinearHeading(new Pose2d(0, 0, 80))
+                 .build();
+
+         toStack3 = drive.trajectoryBuilder(toStack2.end())
+                 .lineToLinearHeading(new Pose2d(0, 0, 70))
+                 .build();
+
     }
 }
