eb4f430bbecb1e3bf07bd50f5f277b2a172b17d6
theo vasile <ajs>
2023-12-21 21:57:38 +0200

    added test for a sequance test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/tests/sequance.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.robots.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Libs.GAMEPAD;
+import org.firstinspires.ftc.teamcode.robots.subSystems.OutTake;
+import org.firstinspires.ftc.teamcode.robots.subSystems.Variables;
+
+@TeleOp(name = "sequanceTest")
+public class sequance extends LinearOpMode {
+    OutTake outTake;
+    ElapsedTime timer;
+    STATES glisieraState = STATES.DONE;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        outTake = new OutTake(hardwareMap);
+        timer = new ElapsedTime();
+        GAMEPAD GAMEPAD = new GAMEPAD(this.gamepad1, telemetry);
+        waitForStart();
+        timer.startTime();
+        while(opModeIsActive() && !isStopRequested()) {
+            if(GAMEPAD.x.toggle) {
+                timer.reset();
+                glisieraState = STATES.WAITING;
+            }
+            if(glisieraState ==  STATES.WAITING && timer.milliseconds() > 2000) {
+                outTake.setBrat(Variables.pivotSus);
+                outTake.setClaw(Variables.pivotSus);
+                glisieraState = STATES.DONE;
+            }
+        }
+    }
+    enum STATES {
+        DONE,
+        WAITING
+    }
+}
